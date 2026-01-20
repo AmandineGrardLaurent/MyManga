4daf12008057bdc3630888de1101b1809bdbab90
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-20 11:13:27 +0100

    refactor: change product container from <section> to <article>

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,9 +1,9 @@
 export default function ItemCard(product) {
   return `
-    <section class="container-card">
+    <article class="container-card">
         <img src=${product.pictureURL} alt=${product.name} />
         <p>${product.name}</p>
         <p>${product.price}</p>
-        <button id="addCartBtn" data-id="${product.id}">Ajouter au panier</button>
-    </section>`;
+        <button class="addCartBtn" data-id="${product.id}">Ajouter au panier</button>
+    </article>`;
 }
