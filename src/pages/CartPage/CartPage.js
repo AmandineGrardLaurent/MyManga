14062769702a97759125e21544da8d827d34a177
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-20 15:39:14 +0100

    feat: add CartPage file and implement displayCart method

---
+++ src/pages/CartPage/CartPage.js
@@ -0,0 +1,17 @@
+import ItemCard from "../../components/ItemCard/ItemCard.js";
+
+export function displayCart(cart, homepageContainer, productsData) {
+  const cartIds = cart.getItemsId().map((id) => parseInt(id));
+
+  homepageContainer.innerHTML = `<h2>Panier</h2> 
+                                <section id="cart-products"></section>
+                                `;
+
+  const sectionCart = document.getElementById("cart-products");
+
+  productsData
+    .filter((p) => cartIds.includes(p.id))
+    .map((p) => {
+      sectionCart.innerHTML += ItemCard(p);
+    });
+}
