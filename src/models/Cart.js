497581042e0eaf24aed89494e44dfcb78599a904
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-21 09:36:22 +0100

    feat: add removeFromCart method and comment Cart class

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,28 +1,47 @@
 export default class Cart {
+  // Constructeur : initialise le panier à partir du localStorage (ids)
+  // ou crée un tableau vide si rien n'existe encore
   constructor() {
     this.itemsId = JSON.parse(localStorage.getItem("cart")) || [];
   }
 
-  // Fonction pour ajouter un produit au panier
+  // Fonction pour ajouter un article au panier
   addToCart(productId) {
     // On récupère le panier dans le navigateur (à jour)
     this.itemsId = JSON.parse(localStorage.getItem("cart")) || [];
 
-    // On vérifie si le produit est déjà présent dans le panier
+    // On vérifie si l'article est déjà présent dans le panier
     if (!this.itemsId.includes(productId)) {
-      // On ajoute l'id du produit dans le panier
+      // On ajoute l'id de l'article dans le panier
       this.itemsId.push(productId);
+
+      // Mise à jour
       localStorage.setItem("cart", JSON.stringify(this.itemsId));
-      alert("Produit ajouté au panier");
+
+      alert("Article ajouté au panier");
     } else {
-      alert("Produit déjà présent dans le panier");
+      alert("Article déjà présent dans le panier");
     }
+  }
+
+  // Fonction pour supprimer un article du panier
+  removeFromCart(productId) {
+    // Filtre l'id de l'article du tableau des ids
+    this.itemsId = this.itemsId.filter((id) => id !== productId);
+
+    // Mise à jour
+    localStorage.setItem("cart", JSON.stringify(this.itemsId));
+
+    alert("Article supprimé");
   }
 
   // Fonction pour vider le panier
   clearCart() {
     this.itemsId = [];
+
+    // Mise à jour
     localStorage.setItem("cart", JSON.stringify(this.itemsId));
+
     alert("Panier vidé");
   }
 
