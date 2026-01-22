b257dc0c75524bf812ba842aec54db3dc3cded41
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-22 08:50:52 +0100

    add comments in helpers.js

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -12,26 +12,29 @@ export function handleClickHomepage({
     // Récupère l'id du produit depuis l'attribut data-id du bouton
     const productId = event.target.dataset.id;
 
-    // Le clic provient du bouton add-cart-btn (ajouter au panier)
+    // Bouton add-cart-btn (ajouter au panier)
     if (event.target.classList.contains("add-cart-btn")) {
-      // Ajout du produit au panier via la classe Cart
       cart.addToCart(productId);
 
-      // Le clic provient du bouton clear-cart-btn (vider le panier)
+      // Bouton clear-cart-btn (vider le panier)
     } else if (event.target.classList.contains("clear-cart-btn")) {
       cart.clearCart();
 
-      // on ré-affiche le panier pour refléter la mise à jour sans recharger la page
+      // Ré-affiche le panier pour refléter la mise à jour sans recharger la page
       CartPage(cart, homepageContainer, productsData);
 
-      // Le clic provient du bouton remove-cart-btn (supprimer du panier)
+      // Bouton remove-cart-btn (supprimer du panier)
     } else if (event.target.classList.contains("remove-cart-btn")) {
       cart.removeFromCart(productId);
 
-      // on ré-affiche le panier pour refléter la mise à jour sans recharger la page
+      // Ré-affiche le panier pour refléter la mise à jour sans recharger la page
       CartPage(cart, homepageContainer, productsData);
+
+      // Bouton details-product-btn (+ détails)
     } else if (event.target.classList.contains("details-product-btn")) {
       ProductDetailsPage(productId, homepageContainer, productsData);
+
+      // Bouton return-btn (retour accueil)
     } else if (event.target.classList.contains("return-btn")) {
       ProductsPage("All", homepageContainer, productsData);
     }
@@ -52,6 +55,7 @@ export function handleClickSidebar({
     const page = event.target.dataset.page;
     const category = event.target.dataset.category;
 
+    // Si c'est une page produit
     if (page == "products") {
       setCurrentPage("products");
 
@@ -59,6 +63,7 @@ export function handleClickSidebar({
       ProductsPage("All", homepageContainer, productsData);
     }
 
+    // Si on a une catégorie
     if (category) {
       setCurrentPage("products");
 
@@ -66,6 +71,7 @@ export function handleClickSidebar({
       ProductsPage(category, homepageContainer, productsData);
     }
 
+    // Si c'est une page panier
     if (page == "cart") {
       setCurrentPage("cart");
 
