a42a5260e706364ffc38cca0b74ba0f0cf15c801
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-22 08:56:53 +0100

    add comments in customer class

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -1,3 +1,6 @@
+// Classe Customer
+// Représente un client de l'application
+// Elle encapsule les informations de base liées à un client
 class Customer {
   constructor(id, firstname, lastname) {
     this.id = id;
@@ -23,12 +26,12 @@ class Customer {
     this.lastname = lastname;
   }
 
-  // Méthode pour récupérer le nom entier
+  // Retourne le nom complet du client (prénom + nom)
   getFullname() {
     return this.firstname + " " + this.lastname;
   }
 
-  // Méthode d'affichage d'un client
+  // Retourne une représentation textuelle du client
   toString() {
     return `Customer n°${this.id} : ${this.firstname} ${this.lastname}`;
   }
