420608b91e5a92362f7dc5e1d45f8ef6be0008da
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-22 08:42:54 +0100

    add comments in sidebar component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,16 @@
+// Composant Sidebar génère le menu latéral de navigation de l'application
+// Permet de naviguer entre :
+// - la liste complète des produits
+// - les catégories de produits
+// - le panier
 export default function Sidebar() {
   return `
-    
       <nav
-        class="container-menu d-flex flex-column bg-light border-end position-fixed vh-100 p-3"
+        class="container-menu d-flex flex-column bg-light border-end position-fixed vh-100 p-3 g-5"
         style="width: 220px;"
       >
         <img src="../../../docs/logo.png" />
-        <button class="btn btn-outline-success w-100 mb-2" data-page="products">
+        <button class="btn btn-success w-100 mb-2" data-page="products">
           Tous les produits
         </button>
         <button
@@ -27,10 +31,9 @@ export default function Sidebar() {
         >
           Accessoires
         </button>
-        <button class="btn btn-outline-danger w-100 mb-2" data-page="cart">
+        <button class="btn btn-danger w-100 mb-2" data-page="cart">
           Panier
         </button>
       </nav>
-    
   `;
 }
