4482a1053442cbb6831220c82285880349cd475d
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-22 09:23:38 +0100

    add comments in productsPage

--- src/pages/ProductsPage/ProductsPage.js
+++ src/pages/ProductsPage/ProductsPage.js
@@ -1,5 +1,7 @@
 import ItemCard from "../../components/ItemCard/ItemCard.js";
 
+// Page Produits
+// Affiche la liste des articles selon une catégorie donnée
 export function ProductsPage(categoryName, container, productsData) {
   // Définir le titre selon la catégorie
   const title =
@@ -8,12 +10,14 @@ export function ProductsPage(categoryName, container, productsData) {
   // On vide le container avant affichage
   container.innerHTML = `
     <h2 class="mb-4">${title}</h2>
-    <div id="products-list" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"></div>
+    <div id="products-list" class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3"></div>
   `;
 
   const section = document.getElementById("products-list");
 
   // Filtrer les produits selon la catégorie
+  // "All" affiche tous les produits
+  // sinon on affiche le nom de la catégorie
   const filteredProducts =
     categoryName === "All"
       ? productsData
@@ -22,7 +26,7 @@ export function ProductsPage(categoryName, container, productsData) {
   // Ajouter les produits dans la section
   filteredProducts.forEach((p) => {
     const col = document.createElement("div");
-    col.className = "col"; // Bootstrap column
+    col.className = "col d-flex justify-content-center";
     col.innerHTML = ItemCard(p, "products");
     section.appendChild(col);
   });
